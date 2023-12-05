f3a3a8f46357d77c081c133ef69a143f217ece34
Hannah Thompson <hannahmthompsonedu@gmail.com>
2023-12-05 14:55:58 -0500

    refactored rolling-dic for js

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,24 @@
-// Put your code here
+console.log("Let's roll some dice, baby!");
+console.log("---------------------------");
+
+
+for (let i = 0; i < 10; i++) {
+
+    const Roll = () => {
+        let dieValue = Math.floor(Math.random() * 10) + 1;
+        let die = dieValue
+        return die;
+    }
+
+    const die1 = Roll();
+    const die2 = Roll();
+
+    let message = (`${die1} + ${die2} = ${die1 + die2}`);
+
+    if (die1 === die2) {
+        message += " DOUBLES!";
+    }
+
+    console.log(message);
+}
+
