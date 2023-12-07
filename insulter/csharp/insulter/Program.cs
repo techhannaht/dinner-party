4ee02b1aeeb33ecbb5b580afd7211e23001425a8
Hannah Thompson <hannahmthompsonedu@gmail.com>
2023-12-07 13:50:06 -0500

    refactoring insulter to c#

--- insulter/csharp/insulter/Program.cs
+++ insulter/csharp/insulter/Program.cs
@@ -1,25 +1,35 @@
-﻿string[] insults = { "You look like what morning breath smells like.",
+﻿namespace Insulter
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] insults = {  "You look like what morning breath smells like.",
     "If you tried to give me cpr I would probably throw myself back under water",
     "I am not a fan of you",
     "You think you're so special...and you are. The fact of your existence is nearly impossible to believe",
     "I'd rather walk than be on the same plane as you",
     "If I were given a million dollars to hang out with you...I would, but then I'd have to spend half of it on therapy",
-    "Do you have to be so...like that?"};
+    "Do you have to be so...like that?"
+    };
 
-string[] indexes;
-  while (indexes.length < 3) {
-    int candidate = getRandomInt(0, insults[1] - 1);
-    if (!indexes.includes(candidate)) {
-      indexes.push(candidate);
+        List<int> indexes = new List<int>();
+        Random random = new Random();
+
+        while (indexes.Count < 3)
+        {
+            int candidate = random.Next(0, insults.Length);
+            if (!indexes.Contains(candidate))
+            {
+                indexes.Add(candidate);
+            }
+        }
+
+        foreach (int index in indexes)
+        {
+            Console.WriteLine(insults[index]);
+        }
     }
   }
 
-  for (let i=0; i<indexes.length; i++) {
-    int index = indexes[i];
-    Console.WriteLine(insults[index]);
-  }
-
-  static void getRandomInt() {
-    var random = new Random();
-    int insultIndex = random.Next();
   }
